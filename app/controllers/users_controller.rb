cdab52c4cb3cdea1a872110eb44f9c55e9ec0f1d
Wesley Reitzfeld <wes.reitzfeld@gmail.com>
2015-03-11 20:54:11 -0400

    Setup the reset password button on users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   use_growlyflash
   respond_to :html, :js
-  before_action :find_user, only: [:edit, :update, :destroy]
+  before_action :find_user, only: [:edit, :update, :destroy, :send_reset_password]
 
   def index
     @users = User.all
@@ -37,6 +37,15 @@ class UsersController < ApplicationController
     else
       flash[:success] = "'#{@user.email}' has been removed"
     end
+  end
+
+  def send_reset_password
+    if !@user.send_reset_password_instructions
+      flash[:error] = "Failed to send reset password instructions for '#{@user.email}'"
+    else
+      flash[:success] = "Reset password instructions send to '#{@user.email}'"
+    end
+    render nothing: true
   end
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
 
   resources :patients, only: [:index]
 
-  resources :users
+  resources :users do
+    member do
+      get :send_reset_password
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
